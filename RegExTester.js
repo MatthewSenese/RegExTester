2137c28fd26fa27e856593ee22c6ef4e22bf3ce3
Matthew Senese <matthewjsenese@lewisu.edu>
2022-02-06 16:10:09 -0600

    Implemented validated program

--- RegExTester.js
+++ RegExTester.js
@@ -1,3 +1,17 @@
-function test() {
-    
+function testRegEx() { 
+    let result = document.querySelector("p");
+    let txt = document.getElementById("txt").value;
+    let regex = new RegExp(document.getElementById("exp").value);
+
+    console.log(regex.test(txt));
+
+    if (regex.test(txt)){
+        result.innerText = "True";
+    }
+
+    else{
+        result.innerText = "False";
+    }
+
+
 }
